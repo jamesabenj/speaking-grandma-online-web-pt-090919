40943ffe2e2a689c2d1c35f3f0b06fcbfd87e8ca
James Benjamin <james.abenj@gmail.com>
2019-09-10 18:41:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,6 +7,7 @@ def speak_to_grandma(phrase)
     puts "NO, NOT SINCE 1938!"
   else puts "I LOVE YOU TOO PUMPKIN!"
 end
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
